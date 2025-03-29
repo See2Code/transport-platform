b9ec7ba8889b9d12cca90502facedc62f9da585d
Robert Sitar <156858274+See2Code@users.noreply.github.com>
2025-03-29 06:46:17 +0100

    - Presunúť všetky interfaces do samostatných súborov v `src/types/`

---
+++ src/constants/countries.ts
@@ -0,0 +1,10 @@
+import { Country } from '../types/contact';
+
+export const countries: Country[] = [
+  { code: 'sk', name: 'Slovensko', prefix: '+421' },
+  { code: 'cz', name: 'Česko', prefix: '+420' },
+  { code: 'hu', name: 'Maďarsko', prefix: '+36' },
+  { code: 'pl', name: 'Poľsko', prefix: '+48' },
+  { code: 'at', name: 'Rakúsko', prefix: '+43' },
+  { code: 'de', name: 'Nemecko', prefix: '+49' },
+]; 
